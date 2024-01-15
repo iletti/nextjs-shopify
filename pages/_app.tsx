6d0f257806585e7ae14d44161dd851df7a005d58
Ilari Schmidt <ilari@digilari.com>
2024-01-15 12:39:29 +0200

    Added GA 4 tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,21 @@
-import { FC } from 'react'
-import type { AppProps } from 'next/app'
-import Layout from '@components/common/Layout'
-import { builder, Builder } from '@builder.io/react'
-import builderConfig from '@config/builder'
-builder.init(builderConfig.apiKey)
+import { FC, useEffect } from 'react';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import Layout from '@components/common/Layout';
+import { builder, Builder } from '@builder.io/react';
+import builderConfig from '@config/builder';
 
-import '@builder.io/widgets'
-import '../blocks/ProductGrid/ProductGrid.builder'
-import '../blocks/CollectionView/CollectionView.builder'
-import '../blocks/ProductView/ProductView.builder'
-import '../blocks/CloudinaryImage/CloudinaryImage.builder'
+// Initialize the Builder.io SDK
+builder.init(builderConfig.apiKey);
 
+// Import your Builder.io widgets and custom components
+import '@builder.io/widgets';
+import '../blocks/ProductGrid/ProductGrid.builder';
+import '../blocks/CollectionView/CollectionView.builder';
+import '../blocks/ProductView/ProductView.builder';
+import '../blocks/CloudinaryImage/CloudinaryImage.builder';
+
+// Register insert menus for Builder.io components
 Builder.register('insertMenu', {
   name: 'Shopify Collections Components',
   items: [
@@ -18,7 +23,7 @@ Builder.register('insertMenu', {
     { name: 'ProductCollectionGrid' },
     { name: 'CollectionView' },
   ],
-})
+});
 
 Builder.register('insertMenu', {
   name: 'Shopify Products Components',
@@ -27,21 +32,42 @@ Builder.register('insertMenu', {
     { name: 'ProductBox' },
     { name: 'ProductView' },
   ],
-})
+});
 
 Builder.register('insertMenu', {
   name: 'Cloudinary Components',
   items: [{ name: 'CloudinaryImage' }],
-})
+});
 
-const Noop: FC<{ children: React.ReactNode }> = ({ children }) => (
-  <>{children}</>
-)
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    // Place any additional GA4 initialization logic here if needed
+  }, []);
 
-export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout pageProps={pageProps}>
-      <Component {...pageProps} />
-    </Layout>
-  )
-}
+    <>
+      {/* Google Analytics Scripts */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      
+      {/* Layout and Page Component */}
+      <Layout pageProps={pageProps}>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+};
+
+export default MyApp;
